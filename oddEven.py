48997c857045c6088cffae6c329d769b61828840
jyotsana <jyotsanashyama@hotmail.com>
2022-10-10 02:31:50 +0530

    first commit

---
+++ oddEven.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter a number:"))
+
+
+if num1 % 2 == 0:
+    print("The input number is an even number.")
+
+elif num1 % 2 !=0:
+    print("The input number is an odd number.")
+
+
