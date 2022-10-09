fa53e13556567e7d7e5f8b85765cfce2c0186b58
jyotsana <jyotsanashyama@hotmail.com>
2022-10-10 02:37:58 +0530

    first commit

---
+++ changeValue.py
@@ -0,0 +1,13 @@
+#from tempfile import tempdir
+#change the input numbers using third variable
+
+num1 = input("Enter 1st number:")
+num2 = input("Enter 2nd number:")
+
+temp = num1
+num1 = num2
+num2 = temp
+print("The value of x is:",num1)
+print("The value of y is:",num2) 
+
+
