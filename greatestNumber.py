59878b82d8b379e27be619722e5c10967effcfcc
jyotsana <jyotsanashyama@hotmail.com>
2022-10-09 21:55:48 +0530

    first commit

---
+++ greatestNumber.py
@@ -0,0 +1,11 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if num1 > num2:
+    print("First number is greatest.")
+
+elif num2 > num1:
+    print("Second number is greatest.")
+
+else:
+    print("Both numbers are equal.")
