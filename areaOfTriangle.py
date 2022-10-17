3ae9e1bd21f814d73bd3662040d0875f15c43035
jyotsana <jyotsanashyama@hotmail.com>
2022-10-17 20:28:04 +0530

    commit

---
+++ areaOfTriangle.py
@@ -0,0 +1,6 @@
+a = float(input("Enter first number: "))
+b= float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+s = (a+b+c)/2 #semiperimeter of traingle
+area = (s(s-a)*(s-b)*(s-c))**0.5
+print("Area of triangle with given sides: ",area)
