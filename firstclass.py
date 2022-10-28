199d8f99e84b51ea6c288002ce6ed45a6e516ce4
jyotsana <jyotsanashyama@hotmail.com>
2022-10-28 23:26:29 +0530

    commit

---
+++ firstclass.py
@@ -0,0 +1,30 @@
+from operator import truediv
+#camelcasing
+#variables
+#global & local variables
+print("Hello world")
+# not applicable in python
+#int x= 5
+#float y= 10.5
+#bool z = true/False
+#string a = "this is string"
+
+ #this is applicable in python
+ #x = 5
+ #y= 10.5
+ #z=true
+ #a= "this is a string"
+
+x = 1000
+y = 92.2
+z = "Hello world! I am Jyotsana Shyama"
+
+print(x)
+print(y)
+print(z)
+
+firstName = "Jyotsana"
+secondName="Vaishnavi"
+thirdName= "Vanshika"
+
+print(firstName,secondName,thirdName)
