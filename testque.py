02bc7da8f626855ad498e76f0a61dc8817a095d4
jyotsana <jyotsanashyama@hotmail.com>
2022-10-28 23:25:24 +0530

    commit

---
+++ testque.py
@@ -0,0 +1,16 @@
+s="Python is a programming language"
+a=str(input("a="))
+b=str(input("b="))
+c=str(input("c="))
+if a in s:
+    print("true")
+else:
+    print("false")
+if b in s:
+    print("true")
+else:
+    print("false")
+if c in s:
+    print("true")
+else:
+    print("false")
